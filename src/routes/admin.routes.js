26db701224a739666ab2556db49773beda208f36
MerajAli1 <merajali45425@gmail.com>
2025-01-01 01:42:34 -0800

    Added some commits

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -9,12 +9,12 @@ import { updateStudent } from "../controllers/admin.controller.js"
 
 const router = Router();
 
-router.route("/adminSignup").post(handleAdminSignUp);
-router.route("/adminLogin").post(handleAdminLogin);
-router.route("/adminHomePage").get(handleAdminHomePage);
-router.route("/deleteAlumni/:id").delete(deleteAlumni);
-router.route("/deleteStudent/:id").delete(deleteStudent);
-router.route("/updateAlumni/:id").put(updateAlumni);
-router.route("/updateStudent/:id").put(updateStudent);
+router.route("/adminSignup").post(handleAdminSignUp);//http://localhost:5000/api/user/adminSignup
+router.route("/adminLogin").post(handleAdminLogin);//http://localhost:5000/admin/adminLogin
+router.route("/adminHomePage").get(handleAdminHomePage);//http://localhost:5000/api/user/adminHomePage
+router.route("/deleteAlumni/:id").delete(deleteAlumni);//http://localhost:5000/api/user/deleteAlumni/:id
+router.route("/deleteStudent/:id").delete(deleteStudent);//http://localhost:5000/api/user/deleteStudent/:id
+router.route("/updateAlumni/:id").put(updateAlumni);//http://localhost:5000/api/user/updateAlumni/:id
+router.route("/updateStudent/:id").put(updateStudent);//http://localhost:5000/api/user/updateStudent/:id
 
 export default router;
